529455d542ad124810aaedeabf18b0ee6892e8dc
Benjamin Tan <jerchienbenjamin@gmail.com>
2022-03-22 10:40:53 +0800

    Implemented extend base.html for navbar and footer

--- README.txt
+++ README.txt
@@ -1,7 +1,9 @@
-to run server: 
+Developmental Django server for Joy Studio 
+
+To run server: 
 python manage.py runserver
 
 
-When changing anything in models.py:
+When changing anything in models.py or upon inital deployment:
 python manage.py makemigrations 
 python manage.py migrate
--- myapp/urls.py
+++ myapp/urls.py
@@ -13,11 +13,8 @@ urlpatterns = [
     path('post/<str:pk>', views.post, name="post"),
     path('calc', views.calc, name="calc"),
     path('profile/change-password', views.change_password, name ="change_password"),
-    path('profile/reset-password', PasswordResetView.as_view(), name ="reset_password"),
-    path('profile/reset-password/done', PasswordResetDoneView.as_view(), name="password_reset_done"),
-    """path(r'^profile/reset-password/confirm/(?P<uid>[0-9A-Za-z]+)-(?P<token>.+)/$', #used regular expresssion idk wth this even means
-    PasswordResetConfirmView.as_view(), name="password_reset_confirm"), 
-    path()"""
+    
+   
     
     
     ]
